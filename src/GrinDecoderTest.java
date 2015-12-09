00102d0d7c0bfb681992070455548855e2abd02d
Prabir Pradhan <prabirmsp@gmail.com>
2015-12-09 16:04:10 -0600

    less than oseras!

--- src/GrinDecoderTest.java
+++ src/GrinDecoderTest.java
@@ -9,7 +9,9 @@ public class GrinDecoderTest {
     public void testDecode() throws Exception {
         //GrinEncoder.encode("huffman-example.txt", "huffman-example.grin");
         //GrinDecoder.decode("huffman-example.grin", "test1.txt");
-        GrinEncoder.encode("wikipedia-huffman-coding.txt", "wiki-example1.grin");
-        GrinDecoder.decode("wiki-example1.grin", "test3.txt");
+        //GrinEncoder.encode("wikipedia-huffman-coding.txt", "wiki-example1.grin");
+        //GrinDecoder.decode("wiki-example1.grin", "test3.txt");
+        GrinEncoder.encode("pg2600.txt", "pg.grin");
+        GrinDecoder.decode("pg.grin", "test3.txt");
     }
 }
